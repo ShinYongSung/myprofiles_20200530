5bd197d15eda0e497b8769b16161d82e016219f8
ShinYongSUng <newxm@naver.com>
2020-05-30 12:37:43 +0900

    전화연결 구현

---
+++ app/src/main/java/my/kim/myprofiles_20200530/EditNickNameActivity.kt
@@ -0,0 +1,15 @@
+package my.kim.myprofiles_20200530
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+    }
+
+
+}
--- app/src/main/java/my/kim/myprofiles_20200530/MainActivity.kt
+++ app/src/main/java/my/kim/myprofiles_20200530/MainActivity.kt
@@ -1,12 +1,42 @@
 package my.kim.myprofiles_20200530
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_FOR_NICKNAME = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeNickBtn.setOnClickListener {
+
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
+        }
+
+        dialBtn.setOnClickListener {
+            val phoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:${phoneNum.replace("-", "-")}")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_FOR_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
+                val
+            }
+        }
     }
 }
